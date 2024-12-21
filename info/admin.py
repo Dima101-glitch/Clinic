5aa87d1070c97d43872da7c2ecdce23af93aa1b9
dimas <dima.shyriaiev.i@gmail.com>
2024-12-21 17:05:13 +0100

    Direction and Doctors models creations

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from info.models import Direction, Doctor
 
-# Register your models here.
+
+@admin.register(Direction)
+class DirectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'sort_number')
+    list_editable = ('sort_number',)
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'slug')
+    list_per_page = 10
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'birth_date', 'experience', 'sort_number')
+    list_editable = ('sort_number',)
+    list_filter = ('directions', 'experience', 'birth_date')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ('name', 'slug', 'description')
+    filter_horizontal = ('directions',)
+    list_per_page = 10
--- info/models.py
+++ info/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Direction(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    sort_number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.slug
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    directions = models.ManyToManyField(Direction)
+
+    description = models.TextField()
+
+    birth_date = models.DateField()
+
+    experience = models.PositiveIntegerField()
+    sort_number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.slug} | {self.birth_date}"
